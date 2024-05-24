db9d04c73d53d48d3744e9ba37f2c222e167fc2d
Simon Carter <simon@somar.co.nz>
2024-05-24 13:08:56 +1200

    Add missing import

--- _config.php
+++ _config.php
@@ -1,5 +1,7 @@
 <?php
 
+use SilverStripe\View\Parsers\ShortcodeParser;
+
 define('CHARTS_DIR', basename(__DIR__));
 
 ShortcodeParser::get('default')->register('chart', ['ChartExtension', 'chartShortcodeHandler']);
